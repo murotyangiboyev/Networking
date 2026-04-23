b7175219682fa3593b607b9965104e8c80b4b944
Murot Yangiboyev <murotyangiboyev77@gmail.com>
2026-04-23 09:36:23 +0500

    OSI model

---
+++ src/main/java/org/example/networking/NetworkingApplication.java
@@ -0,0 +1,13 @@
+package org.example.networking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/networking/NetworkingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.networking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
